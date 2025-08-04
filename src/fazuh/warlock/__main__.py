3d3867d4dc0ea08ac5072b9a84fa927b92e0c7ba
FAZuH <fazuhhh@proton.me>
2025-08-04 09:21:55 +0700

    add log file rotation

--- src/fazuh/warlock/__main__.py
+++ src/fazuh/warlock/__main__.py
@@ -1,4 +1,8 @@
+from loguru import logger
+
+
 def main():
+    logger.add("log/{time}.log", rotation="1 day")
     from fazuh.warlock.module.update_tracker import UpdateTracker
 
     UpdateTracker().start()
